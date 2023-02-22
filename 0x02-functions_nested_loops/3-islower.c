1c34d56e5b5467f6e6566f74b57c527f01ac0286
‘GeorginaMampuru’ <georginamampru@gmail.com>
2023-02-22 01:46:38 -0800

    Shower M

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,8 +4,8 @@
 
 #include "main.h"
 
-/*
- *Description: _islower - Checks if a character is lowercase.
+/**
+ * _islower - Checks if a character is lowercase.
  * @c: The character to be checked.
  *
  * Return: 1 if character is lowercase, 0 otherwise.
