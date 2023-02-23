dc25d6f95aa07fded1d61e1b168728f9e4842252
‘GeorginaMampuru’ <georginamampru@gmail.com>
2023-02-23 12:49:22 -0800

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+*/
+
+#include "stdlib.h"
+#include "main.h"
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
+
